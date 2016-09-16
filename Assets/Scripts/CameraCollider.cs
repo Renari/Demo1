c19c1b13a7e00e9ebf3a412096a72e15a3e9a51f
Renari <renari@arimil.com>
2016-09-16 00:24:56 -0400

    add plane and background to game scene

---
+++ Assets/Scripts/CameraCollider.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraCollider : MonoBehaviour {
+
+    private GameObject topColliderObject;
+    private GameObject leftColliderObject;
+    private GameObject rightColliderObject;
+    private GameObject bottomColliderObject;
+
+    private EdgeCollider2D topCollider;
+    private EdgeCollider2D leftCollider;
+    private EdgeCollider2D rightCollider;
+    private EdgeCollider2D bottomCollider;
+
+    // Use this for initialization
+    void Start () {
+        topColliderObject = new GameObject();
+        leftColliderObject = new GameObject();
+        rightColliderObject = new GameObject();
+        bottomColliderObject = new GameObject();
+
+        topCollider = topColliderObject.AddComponent<EdgeCollider2D>();
+        leftCollider = leftColliderObject.AddComponent<EdgeCollider2D>();
+        rightCollider = rightColliderObject.AddComponent<EdgeCollider2D>();
+        bottomCollider = bottomColliderObject.AddComponent<EdgeCollider2D>();
+
+        topColliderObject.name = "TopCollider";
+        leftColliderObject.name = "LeftCollider";
+        rightColliderObject.name = "RightCollider";
+        bottomColliderObject.name = "BottomCollider";
+
+        topColliderObject.transform.SetParent(transform);
+        leftColliderObject.transform.SetParent(transform);
+        rightColliderObject.transform.SetParent(transform);
+        bottomColliderObject.transform.SetParent(transform);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 topLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
+        Vector3 topRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
+        Vector3 bottomLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, 0));
+        Vector3 bottomRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0));
+
+        topColliderObject.transform.position = topLeft;
+        leftColliderObject.transform.position = bottomLeft;
+        rightColliderObject.transform.position = bottomRight;
+        bottomColliderObject.transform.position = bottomLeft;
+
+        topCollider.points = new Vector2[]
+        {
+            new Vector2(0, 0),
+            new Vector2(topRight.x - topLeft.x, 0)
+        };
+        leftCollider.points = new Vector2[]
+        {
+            new Vector2(0, 0),
+            new Vector2(0, topLeft.y - bottomLeft.y)
+        };
+        rightCollider.points = new Vector2[]
+        {
+            new Vector2(0, 0),
+            new Vector2(0, topRight.y - bottomRight.y)
+        };
+        bottomCollider.points = new Vector2[]
+        {
+            new Vector2(0, 0),
+            new Vector2(bottomRight.x - bottomLeft.x, 0)
+        };
+    }
+}
---
+++ Assets/Scripts/PlaneController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneController : MonoBehaviour {
+
+    private const float MOVEMENT_SPEED = 0.05f;
+    private Transform planeTransform;
+
+	// Use this for initialization
+	void Start () {
+        planeTransform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            planeTransform.position = new Vector3(planeTransform.position.x - MOVEMENT_SPEED, planeTransform.position.y, planeTransform.position.z);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            planeTransform.position = new Vector3(planeTransform.position.x + MOVEMENT_SPEED, planeTransform.position.y, planeTransform.position.z);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            planeTransform.position = new Vector3(planeTransform.position.x, planeTransform.position.y + MOVEMENT_SPEED, planeTransform.position.z);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            planeTransform.position = new Vector3(planeTransform.position.x, planeTransform.position.y - MOVEMENT_SPEED, planeTransform.position.z);
+        }
+    }
+}
